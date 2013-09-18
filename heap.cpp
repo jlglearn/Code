3cfa090ecb9af0136bab8ed614b648c2f016a0ef
José Luis González <jlg@ZHODB.(none)>
2013-09-18 10:30:06 -0500

    Add HEAP (heap.cpp, heap.h)

--- heap.cpp
+++ heap.cpp
@@ -1,174 +1,122 @@
-#include <memory>
 #include <cstring>
+#include <string>
 #include <cstdlib>
-#include <iostream>
 #include "debug.h"
 #include "heap.h"
 
-#define HEAPLeft(i) ((i)*2)
-#define HEAPRight(i) ((i)*2+1)
-#define HEAPParent(i) ((i)/2)
+#define DEFNUMELEMENTS 512
 
-Heap::Heap(int size)
+#define HEAPLEFT(i) (i*2)
+#define HEAPRIGHT(i) (i*2+1)
+#define HEAPPARENT(i) (i/2)
+#define HEAPELEMENT(i) (pE[i-1])
+
+
+static void HeapSwap( HeapElement *p, int i, int j )
 {
-    int n;
-
-    // round up size to an even power of 2
-    for( n = 1024; n < size; n *= 2 );
-    
-    pHeap = (HeapNode *)malloc(n*sizeof(HeapNode));
-    heapSize = n;
-    heapLength = 0;
+    HeapElement t;
+    memcpy((void *)&t, (void *)&p[i-1], sizeof(HeapElement));
+    memcpy((void *)&p[i-1], (void *)&p[j-1], sizeof(HeapElement));
+    memcpy((void *)&p[j-1], (void *)&t, sizeof(HeapElement));
 }
 
-Heap::Heap(void)
+HEAP::HEAP(void)
 {
-    pHeap = (HeapNode *)malloc(1024*sizeof(HeapNode));
-    heapSize = 1024;
-    heapLength = 0;
+    nElements = 0;
+    size = 0;
+    pE = (HeapElement *)0;
+    resize();
 }
 
-Heap::~Heap(void)
+HEAP::~HEAP(void)
 {
-    if ( pHeap )
+    if (pE) free(pE);
+}
+
+void HEAP::HeapifyUp(int i)
+{
+    while (i > 1)
     {
-        free(pHeap);
-    }
-}
-
-int Heap::Insert(int key, void *pData )
-{
-    int i, p;
-    
-    if (heapLength >= heapSize)
-        resize();
+        int p = HEAPPARENT(i);
         
-    pHeap[heapLength].key = key;
-    pHeap[heapLength].pData = pData;
-    heapLength++;
-    
-    for( i = heapLength; i != 1; i = HEAPParent(i) )
-    {
-        int p = HEAPParent(i),
-            ki = E(i),
-            kp = E(p);
-        
-        if (ki > kp)
+        if (HEAPELEMENT(p).key <= HEAPELEMENT(i).key)
         {
-            // child strictly bigger than parent, done
-            return 1;
-        }
-        
-        if (ki < kp)
-        {
-            // child less than parent, swap
-            Swap(i,p);
-            // and continue checking upward
+            // if parent <= than child (i), heap holds, done
+            break;
         }
         else
         {
-            // child equal to parent, make sure it's parent's left child
-            if (i != HEAPLeft(p))
-            {            
-                // child is equal to parent, and is parent's right child.  swap children
-                // so that equal values are on left subtree
-                Swap(i,HEAPLeft(p));
-                // and make sure we still have a valid heap on the right branch
-                Heapify(i);
-            }
-            
-            return 1;
+            // if parent greater than child, swap and continue checking upward
+            HeapSwap(pE, p, i);
         }
+        
+        i = p;
     }
-    
-    return 1;
 }
 
-int Heap::Length(void)
+void HEAP::HeapifyDown(int i)
 {
-    return heapLength;
-}
-
-int Heap::Peek(void)
-{
-    ASSERT(heapLength>0,"Attempting to peek empty heap");
-    return pHeap[0].key;
-}
-
-void * Heap::Pop(void)
-{
-    HeapNode t;
-    ASSERT(heapLength>0,"Attempting to pop empty heap");
-    t = pHeap[0];
-    Swap(1,heapLength);
-    heapLength--;
-    Heapify(1);
+    if (i >= nElements)
+        return;
+        
+    int l = HEAPLEFT(i);
+    int r = HEAPRIGHT(i);
+    int m = i;
     
-    return t.pData;
-}
-
-int Heap::E(int i)
-{
-    ASSERT(i <= heapLength, "Invalid index in Heap::E");
-    ASSERT(i > 0, "Invalid index 0 in Heap::E");
-    return pHeap[i-1].key;
-}
-
-void Heap::Swap(int i, int j)
-{
-    HeapNode t;
-    
-    ASSERT((i>0)&&(i<=heapLength),"Invalid index i in Heap::Swap");
-    ASSERT((j>0)&&(j<=heapLength),"Invalid index j in Heap::Swap");
-    
-    t = pHeap[i-1];
-    pHeap[i-1] = pHeap[j-1];
-    pHeap[j-1] = t;
-}
-
-void Heap::Heapify(int i)
-{
-    int m, l, r;
-    
-    ASSERT((i>0) && (i <= heapLength),"Invalid index in Heapify");
-    
-    l = HEAPLeft(i);
-    r = HEAPRight(i);
-    m = i;
-    
-    if ((l<=heapLength) && (E(l) < E(i)))
+    if ((l <= nElements) && (HEAPELEMENT(l).key < HEAPELEMENT(i).key))
         m = l;
-    if ((r<=heapLength) && (E(r) < E(m)))
+        
+    if ((r <= nElements) && (HEAPELEMENT(r).key < HEAPELEMENT(m).key))
         m = r;
         
-    if (m != i)
-    {
-        Swap(i,m);
-        Heapify(m);
-    }
+    if (m == i)
+        return;
+        
+    HeapSwap(pE, m, i);
+    HeapifyDown(m);
 }
 
-void Heap::resize()
+void HEAP::insertElement(int key, void *pData)
 {
-    int newSize = heapSize*2;
-    HeapNode *pNew = (HeapNode *) malloc(newSize * sizeof(HeapNode));
-    
-    if (pNew)
-    {
-        memset((void *) pNew, 0, newSize*sizeof(HeapNode));
-        memcpy((void *) pNew, (void *) pHeap, heapSize*sizeof(HeapNode));
-        free(pHeap);
-        pHeap = pNew;
-        heapSize = newSize;
-    }
-    
+    if (nElements >= size)
+        resize();
+        
+    pE[nElements].key = key;
+    pE[nElements].pData = pData;
+    nElements++;
+    HeapifyUp(nElements);
 }
 
-void Heap::Dump()
+void * HEAP::getData(void)
 {
-    for( int i = 0; i < heapLength; i++ )
+    return ((nElements > 0) ? pE[0].pData : (void *)0);
+}
+
+int HEAP::Pop(void)
+{
+    ASSERT(nElements > 0, "Pop() on empty Heap");
+    if (nElements > 0)
     {
-        std::cout << pHeap[i].key << " ";
+        int k = pE[0].key;
+        HeapSwap(pE, 1, nElements);
+        nElements--;
+        HeapifyDown(1);
+        return k;
     }
-    std::cout << "\n";
+    return 0;
+}
+
+
+void HEAP::resize(void)
+{
+    int newsize = ((size == 0) ? DEFNUMELEMENTS : size * 2);
+    HeapElement *pNew = (HeapElement *) malloc(sizeof(HeapElement) * newsize);
+    memset((void *)pNew, 0, sizeof(HeapElement) * newsize);
+    if (pE)
+    {
+        memcpy((void *)pNew, (void *)pE, sizeof(HeapElement) * size);
+        free(pE);
+    }
+    pE = pNew;
+    size = newsize;
 }
--- heap.h
+++ heap.h
@@ -1,35 +1,122 @@
-#ifndef _HEAP_H_
-#define _HEAP_H_
+#include <cstring>
+#include <string>
+#include <cstdlib>
+#include "debug.h"
+#include "heap.h"
+
+#define DEFNUMELEMENTS 512
+
+#define HEAPLEFT(i) (i*2)
+#define HEAPRIGHT(i) (i*2+1)
+#define HEAPPARENT(i) (i/2)
+#define HEAPELEMENT(i) (pE[i-1])
 
 
-struct structHeapNode {
-    int key;
-    void *pData;
-};
+static void HeapSwap( HeapElement *p, int i, int j )
+{
+    HeapElement t;
+    memcpy((void *)&t, (void *)&p[i-1], sizeof(HeapElement));
+    memcpy((void *)&p[i-1], (void *)&p[j-1], sizeof(HeapElement));
+    memcpy((void *)&p[j-1], (void *)&t, sizeof(HeapElement));
+}
 
-typedef structHeapNode HeapNode;
+HEAP::HEAP(void)
+{
+    nElements = 0;
+    size = 0;
+    pE = (HeapElement *)0;
+    resize();
+}
 
-class Heap {
-public:
-    Heap(void);
-    Heap(int size);
-    ~Heap(void);
+HEAP::~HEAP(void)
+{
+    if (pE) free(pE);
+}
+
+void HEAP::HeapifyUp(int i)
+{
+    while (i > 1)
+    {
+        int p = HEAPPARENT(i);
+        
+        if (HEAPELEMENT(p).key <= HEAPELEMENT(i).key)
+        {
+            // if parent <= than child (i), heap holds, done
+            break;
+        }
+        else
+        {
+            // if parent greater than child, swap and continue checking upward
+            HeapSwap(pE, p, i);
+        }
+        
+        i = p;
+    }
+}
+
+void HEAP::HeapifyDown(int i)
+{
+    if (i >= nElements)
+        return;
+        
+    int l = HEAPLEFT(i);
+    int r = HEAPRIGHT(i);
+    int m = i;
     
-    int Insert(int key, void *pData);
-    int Peek(void);
-    void * Pop(void);
-    int Length(void);
-    void Dump(void);
-    
-private:
-    HeapNode *pHeap;
-    int heapSize;
-    int heapLength;
-    
-    void Swap(int i, int j);
-    int E(int i);
-    void Heapify(int i);
-    void resize(void);
-};
+    if ((l <= nElements) && (HEAPELEMENT(l).key < HEAPELEMENT(i).key))
+        m = l;
+        
+    if ((r <= nElements) && (HEAPELEMENT(r).key < HEAPELEMENT(m).key))
+        m = r;
+        
+    if (m == i)
+        return;
+        
+    HeapSwap(pE, m, i);
+    HeapifyDown(m);
+}
 
-#endif
+void HEAP::insertElement(int key, void *pData)
+{
+    if (nElements >= size)
+        resize();
+        
+    pE[nElements].key = key;
+    pE[nElements].pData = pData;
+    nElements++;
+    HeapifyUp(nElements);
+}
+
+void * HEAP::getData(void)
+{
+    return ((nElements > 0) ? pE[0].pData : (void *)0);
+}
+
+int HEAP::Pop(void)
+{
+    ASSERT(nElements > 0, "Pop() on empty Heap");
+    if (nElements > 0)
+    {
+        int k = pE[0].key;
+        HeapSwap(pE, 1, nElements);
+        nElements--;
+        HeapifyDown(1);
+        return k;
+    }
+    return 0;
+}
+
+
+void HEAP::resize(void)
+{
+    int newsize = ((size == 0) ? DEFNUMELEMENTS : size * 2);
+    HeapElement *pNew = (HeapElement *) malloc(sizeof(HeapElement) * newsize);
+    memset((void *)pNew, 0, sizeof(HeapElement) * newsize);
+    if (pE)
+    {
+        memcpy((void *)pNew, (void *)pE, sizeof(HeapElement) * size);
+        free(pE);
+    }
+    pE = pNew;
+    size = newsize;
+}
