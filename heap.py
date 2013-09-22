ea85accae17e77289db5818d92c597f5bbd8c406
Jose Luis Gonzalez <jlglearn@gmail.com>
2013-09-22 12:52:27 -0500

    Rewrite Heap()

--- heap.py
+++ heap.py
@@ -1,111 +1,83 @@
-import sys;
+HeapLeft = lambda i : i*2;
+HeapRight = lambda i : i*2+1;
+HeapParent = lambda i : i//2;
 
-A = [];
+class Heap:
 
-HeapLeft = lambda i: 2*i;
-HeapRight = lambda i: 2*i + 1;
-HeapParent = lambda i: i//2;
-
-def HeapElement(i):
-    global A;
-    return A[i-1];
-
-def HeapSwap(i,j):
-    global A;
-    x = A[i-1];
-    A[i-1] = A[j-1];
-    A[j-1] = x;
-    
-
-def MinHeapify(i):
-    global A;
-    
-    l = HeapLeft(i);
-    r = HeapRight(i);
-    
-    m = i; 
-    if (l <= len(A)) and (HeapElement(l) < HeapElement(i)):
-        m = l; 
-    
-    if (r <= len(A)) and (HeapElement(r) < HeapElement(m)):
-        m = r;         
+    def __init__(self):
+        self.A = [];
+        self.nElements = 0;
         
-    if (i != m):
-        HeapSwap(i,m);
-        MinHeapify(m);
-
-def HeapPop():
-    global A;
-    if len(A) > 0:
-        x = A[0];
-        HeapSwap(1,len(A));
-        A.pop();
-        MinHeapify(1);    
+    def Value(self, i):
+        assert((i > 0) and (i <= self.nElements));
+        return self.A[i-1]['key'];
+        
+    def Swap(self, i, j):
+        assert((i > 0) and (i <= self.nElements) and (j > 0) and (j <= self.nElements));
+        t = self.A[i-1];
+        self.A[i-1] = self.A[j-1];
+        self.A[j-1] = t;
+        
+    def HeapifyDown(self, i):
+        
+        l = HeapLeft(i);
+        r = HeapRight(i);
+        
+        m = i;
+        
+        if ((l <= self.nElements) and (self.Value(l) < self.Value(i))):
+            m = l;
+            
+        if ((r <= self.nElements) and (self.Value(r) < self.Value(m))):
+            m = r;
+            
+        if m == i: return;
+        
+        self.Swap(m, i);
+        self.HeapifyDown(m);
+        
+    def HeapifyUp(self, i):
+        if i == 1:
+            return;
+            
+        p = HeapParent(i);
+        l = HeapLeft(p);
+        r = HeapRight(p);
+        
+        m = p;
+        
+        if ((l <= self.nElements) and (self.Value(l) < self.Value(p))):
+            m = l;
+            
+        if ((r <= self.nElements) and (self.Value(r) < self.Value(m))):
+            m = r;
+            
+        if m == p:
+            #parent strictly less than children, done
+            return;
+            
+        self.Swap(m, p);
+        self.HeapifyUp(p);
+        
+    def Element(self, i):
+        return self.A[i-1];
+        
+    def Insert(self, key, data):
+        self.A.append({'key':key, 'data':data});
+        self.nElements += 1;
+        self.HeapifyUp(self.nElements);
+        
+    def Empty(self):
+        return self.nElements == 0;
+        
+    def Pop(self):
+        assert(self.nElements > 0);
+        x = self.Element(1);
+        self.Swap(1, self.nElements);
+        del self.A[self.nElements-1];
+        self.nElements -= 1;
+        self.HeapifyDown(1);
         return x;
         
-def HeapInsert(k):
-    global A;
-    A.append(k);
-    i = len(A);
-    while i != 1:
-        j = HeapParent(i);
-        ki = HeapElement(i);
-        kj = HeapElement(j);
         
-        if ki > kj:
-            # if child (ki) is greater than its parent (kj), heap holds, done.
-            break;
-        elif ki < kj:
-            # if child (ki) is less than its parent (kj), swap them and continue checking upward
-            HeapSwap(i, j);
-        else:
-            # if child (ki) is equal to its parent (kj), make sure it's on parent's left subtree (by convention)
-            l = HeapLeft(j);
-            
-            if i == HeapLeft(j):
-                #indeed left child, done
-                break;
-            else:
-                #swap left and right children of parent, we are now done
-                HeapSwap(i,l);
-                break;
         
-        i = j;
-
-def MakeMinHeap():
-    global A;
-        
-    i = len(A)//2;
-    
-    while i > 0:
-        MinHeapify(i);
-        i -= 1;
-
-def LoadFile(filename):
-    global A;
-    with open(filename,'r') as f:
-        for l in f:
-            for x in l.split():
-                A.append(int(x));
-    f.close();
-    
-def WriteFile(A,filename):
-    with open(filename, 'w') as f:
-        for x in A:
-            f.write("{}\n".format(x));
-            
-def PrintSequence():
-    global A;
-    s1 = "";
-    s2 = "";
-    
-    for i in range(len(A)):
-        s1 += "|{0:3d}".format(i+1);
-        s2 += "|{0:3d}".format(A[i]);
-        
-        if (i%16) == 15:
-            print s1 + "\n" + s2 + "\n";
-            s1 = "";
-            s2 = "";
-
-            
